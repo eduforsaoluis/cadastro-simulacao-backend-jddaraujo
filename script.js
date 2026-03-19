c66cf5165a205a9f602024d419c7fe457c610e96
Davi Araújo <jddaraujo10@gmail.com>
2026-03-19 20:54:02 -0300

    alteração para API

--- script.js
+++ script.js
@@ -1,58 +1,69 @@
-const form = document.getElementById("formCadastro")
-const mensagem = document.getElementById("mensagem")
+const form = document.getElementById("formCadastro");
+const mensagem = document.getElementById("mensagem");
 
-form.addEventListener("submit", function(e) {
-  e.preventDefault()
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-  const nomeInput = document.getElementById("nome")
-  const emailInput = document.getElementById("email")
+  const nomeInput = document.getElementById("nome");
+  const emailInput = document.getElementById("email");
 
-  const nome = nomeInput.value.trim()
-  const email = emailInput.value.trim()
+  const nome = nomeInput.value.trim();
+  const email = emailInput.value.trim();
 
   // limpa erros anteriores
-  nomeInput.classList.remove("erro")
-  emailInput.classList.remove("erro")
+  nomeInput.classList.remove("erro");
+  emailInput.classList.remove("erro");
 
-  let valido = true
+  let valido = true;
 
   if (nome.length < 3) {
-    nomeInput.classList.add("erro")
-    valido = false
+    nomeInput.classList.add("erro");
+    valido = false;
   }
 
   if (!email.includes("@") || email.length < 5) {
-    emailInput.classList.add("erro")
-    valido = false
+    emailInput.classList.add("erro");
+    valido = false;
   }
 
   if (!valido) {
-    mensagem.textContent = "Corrija os campos destacados."
-    mensagem.style.color = "red"
-    return
+    mensagem.textContent = "Corrija os campos destacados.";
+    mensagem.style.color = "red";
+    return;
   }
 
-  mensagem.textContent = "Enviando..."
-  mensagem.style.color = "black"
+  mensagem.textContent = "Enviando...";
+  mensagem.style.color = "black";
+
+  let idade = 42;
+
+  const dados = {
+    nome,
+    email,
+    idade,
+  };
+
+  console.log(dados);
+  console.log(JSON.stringify(dados));
 
   fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       nome,
-      email
+      email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      mensagem.textContent = "Cadastro realizado com sucesso!";
+      mensagem.style.color = "green";
+      console.log(data);
     })
-  })
-  .then(res => res.json())
-  .then(data => {
-    mensagem.textContent = "Cadastro realizado com sucesso!"
-    mensagem.style.color = "green"
-    console.log(data)
-  })
-  .catch(() => {
-    mensagem.textContent = "Erro ao enviar"
-    mensagem.style.color = "red"
-  })
-})
+    .catch(() => {
+      mensagem.textContent = "Erro ao enviar";
+      mensagem.style.color = "red";
+    });
+});
